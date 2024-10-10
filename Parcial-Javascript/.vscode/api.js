b299a9c0efe16b0e2b25e896877ead3730518794
Juan David Rendon B <juan.rendon13@udea.edu.co>
2024-10-09 20:58:36 -0500

    Se añade método createUser en la clase API para crear nuevos usuarios, con su manejo de errores

---
+++ Parcial-Javascript/.vscode/api.js
@@ -0,0 +1,54 @@
+// api.js
+const BASE_URL = "http://ec2-3-138-183-128.us-east-2.compute.amazonaws.com:4010";
+
+export default class API {
+/**
+ * Obtiene una lista de usuarios de la API.
+ *
+ * @param {number} [limit=5] - El número máximo de usuarios a buscar.
+ * @returns {Promise<Object[]>} Una promesa que se resuelve en una matriz de objetos de usuario.
+ * @throws {Error} Si hay un error al buscar los usuarios.
+ */
+  static async getUsers(limit = 5) {
+    try {
+      const response = await fetch(`${BASE_URL}/users?limit=${limit}`);
+      if (!response.ok) {
+        alert("Error al encontrar los usuarios.");
+        throw new Error("Error al encontrar los usuarios.");
+      }
+      return await response.json();
+    } catch (error) {
+      alert("Error al obtener los usuarios");
+      console.error("Error al obtener los usuarios", error);
+    }
+  }
+
+  /**
+ * Crea un nuevo usuario enviando una solicitud POST al servidor.
+ *
+ * @param {Object} user - el objeto de usuario que se creará.
+ * @param {string} user.name - el nombre del usuario.
+ * @param {string} user.email - el correo electrónico del usuario.
+ * @param {string} user.password - la contraseña del usuario.
+ * @returns {Promise<Object>} El objeto de usuario creado.
+ * @throws {Error} Si hay un error al crear el usuario.
+ */
+  static async createUser(user) {
+    try {
+      const response = await fetch(`${BASE_URL}/users`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user),
+      });
+      if (!response.ok) {
+        alert("Error al crear usuario");
+        throw new Error("Error al crear usuario");
+      }
+      alert("Usuario creado con éxito");
+      return await response.json();
+    } catch (error) {
+      alert("Error al crear usuario");
+      console.error("Error al crear el usuario:", error);
+    }
+  }
+}
