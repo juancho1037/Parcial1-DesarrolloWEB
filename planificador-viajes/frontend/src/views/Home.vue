582d36905fcd0c199aefd8fee0463464be0f8592
Juan David Rendon Berrio <juan.rendon13@udea.edu.co>
2024-10-18 20:27:25 -0500

    Inicialización del proyecto Planificador de viajes con Vue.js frontend. Se crean los archivos iniciales.

---
+++ planificador-viajes/frontend/src/views/Home.vue
@@ -0,0 +1,13 @@
+<!-- src/views/Home.vue -->
+<template>
+  <div class="home">
+    <h1>Welcome to Travel Planner</h1>
+    <p>Start planning your next adventure!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
---
+++ planificador-viajes/frontend/src/views/Trips.vue
@@ -0,0 +1,43 @@
+<!-- src/views/Trips.vue -->
+<template>
+  <div class="trips">
+    <h2>My Trips</h2>
+    <ul v-if="trips.length">
+      <li v-for="trip in trips" :key="trip.id">
+        {{ trip.destination }} ({{ trip.startDate }} - {{ trip.endDate }})
+      </li>
+    </ul>
+    <p v-else>No trips planned yet. Start by adding a new trip!</p>
+    <button @click="addTrip">Add New Trip</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Trips",
+  data() {
+    return {
+      trips: [],
+    };
+  },
+  methods: {
+    async fetchTrips() {
+      try {
+        const response = await axios.get("http://localhost:3000/trips");
+        this.trips = response.data;
+      } catch (error) {
+        console.error("Error fetching trips:", error);
+      }
+    },
+    addTrip() {
+      // TODO: Implement add trip functionality
+      console.log("Add trip clicked");
+    },
+  },
+  mounted() {
+    this.fetchTrips();
+  },
+};
+</script>
